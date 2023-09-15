493459d131af9bfe77cb659b9ca210d4a20085b6
Bruno Luis <bruno.vieira@smarketsolutions.com.br>
2023-09-15 10:50:46 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+	projectId: 'qmg57i',
 	chromeWebSecurity: false,
 	e2e: {
 		baseUrl: 'https://notes-serverless-app.com',
