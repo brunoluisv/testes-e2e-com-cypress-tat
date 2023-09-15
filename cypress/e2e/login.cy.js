aaa986b363877022d77e03dcad7c7f6889a9fb3d
Bruno Luis <bruno.vieira@smarketsolutions.com.br>
2023-09-15 10:25:11 -0300

    Create e2e tests with cypress

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,11 @@
+describe('Login', () => {
+	const emailAddress = Cypress.env('USER_EMAIL');
+	const password = Cypress.env('USER_PASSWORD');
+		
+	it('successfully logs in', () => {
+		cy.guiLogin(emailAddress, password);
+
+		cy.contains('h1', 'Your Notes').should('be.visible');
+		cy.contains('a', 'Create a new note').should('be.visible');
+	});
+});
